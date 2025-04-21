ce2cc46e7c9161ab9a876dfdab2ae471763c4092
mdiktushar <md.tushar1804@gmail.com>
2025-04-21 17:41:11 +0600

    route folder structure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -42,7 +42,7 @@ use Symfony\Component\Routing\Exception\RouteNotFoundException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
+        web: __DIR__ . '/../routes/web/web.php',
         api: __DIR__ . '/../routes/api/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
