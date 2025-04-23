fdda71308a1b874f8a4fc522c877a52fa7357696
mdiktushar <md.tushar1804@gmail.com>
2025-04-23 16:33:09 +0600

    removing unwanted contexts

--- app/Http/Controllers/Web/V1/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Web/V1/Auth/AuthenticatedSessionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Web\V1\Auth;
 
 use App\Http\Controllers\Web\V1\Controller;
-use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Web\V1\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
