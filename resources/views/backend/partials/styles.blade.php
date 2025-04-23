02cd9cfcd06cb52614da81d4271a1f0becbe6393
mdiktushar <md.tushar1804@gmail.com>
2025-04-23 11:33:51 +0600

    parcials done

---
+++ resources/views/backend/partials/styles.blade.php
@@ -0,0 +1,7 @@
+<!-- Libs CSS -->
+<link href="../assets/libs/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+<link href="../assets/libs/%40mdi/font/css/materialdesignicons.min.css" rel="stylesheet">
+<link href="../assets/libs/simplebar/dist/simplebar.min.css" rel="stylesheet">
+
+<!-- Theme CSS -->
+<link rel="stylesheet" href="../assets/css/theme.min.css">
