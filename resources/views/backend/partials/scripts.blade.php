46fc0887b84960899d45830cbc04f125f28a69d8
mdiktushar <md.tushar1804@gmail.com>
2025-04-27 18:05:07 +0600

    package install

--- resources/views/backend/partials/scripts.blade.php
+++ resources/views/backend/partials/scripts.blade.php
@@ -9,3 +9,121 @@
 <script src="{{asset('assets/backend/js/theme.min.js')}}"></script>
 <script src="{{asset('assets/backend/libs/apexcharts/dist/apexcharts.min.js')}}"></script>
 <script src="{{asset('assets/backend/js/vendors/chart.js')}}"></script>
+
+
+
+{{-- development ................................................. --}}
+{{-- datatable --}}
+<script src="{{ asset('assets/custom/js/datatables.min.js') }}"></script>
+{{-- toastr --}}
+<script src="{{ asset('assets/custom/js/toastr.min.js') }}"></script>
+<script>
+    $(document).ready(function() {
+        toastr.options.timeOut = 10000;
+        toastr.options.positionClass = 'toast-top-right';
+
+        @if (Session::has('t-success'))
+            toastr.options = {
+                'closeButton': true,
+                'debug': false,
+                'newestOnTop': true,
+                'progressBar': true,
+                'positionClass': 'toast-top-right',
+                'preventDuplicates': false,
+                'showDuration': '1000',
+                'hideDuration': '1000',
+                'timeOut': '5000',
+                'extendedTimeOut': '1000',
+                'showEasing': 'swing',
+                'hideEasing': 'linear',
+                'showMethod': 'fadeIn',
+                'hideMethod': 'fadeOut',
+            };
+            toastr.success("{{ session('t-success') }}");
+        @endif
+
+        @if (Session::has('t-error'))
+            toastr.options = {
+                'closeButton': true,
+                'debug': false,
+                'newestOnTop': true,
+                'progressBar': true,
+                'positionClass': 'toast-top-right',
+                'preventDuplicates': false,
+                'showDuration': '1000',
+                'hideDuration': '1000',
+                'timeOut': '5000',
+                'extendedTimeOut': '1000',
+                'showEasing': 'swing',
+                'hideEasing': 'linear',
+                'showMethod': 'fadeIn',
+                'hideMethod': 'fadeOut',
+            };
+            toastr.error("{{ session('t-error') }}");
+        @endif
+
+        @if (Session::has('t-info'))
+            toastr.options = {
+                'closeButton': true,
+                'debug': false,
+                'newestOnTop': true,
+                'progressBar': true,
+                'positionClass': 'toast-top-right',
+                'preventDuplicates': false,
+                'showDuration': '1000',
+                'hideDuration': '1000',
+                'timeOut': '5000',
+                'extendedTimeOut': '1000',
+                'showEasing': 'swing',
+                'hideEasing': 'linear',
+                'showMethod': 'fadeIn',
+                'hideMethod': 'fadeOut',
+            };
+            toastr.info("{{ session('t-info') }}");
+        @endif
+
+        @if (Session::has('t-warning'))
+            toastr.options = {
+                'closeButton': true,
+                'debug': false,
+                'newestOnTop': true,
+                'progressBar': true,
+                'positionClass': 'toast-top-right',
+                'preventDuplicates': false,
+                'showDuration': '1000',
+                'hideDuration': '1000',
+                'timeOut': '5000',
+                'extendedTimeOut': '1000',
+                'showEasing': 'swing',
+                'hideEasing': 'linear',
+                'showMethod': 'fadeIn',
+                'hideMethod': 'fadeOut',
+            };
+            toastr.warning("{{ session('t-warning') }}");
+        @endif
+    });
+</script>
+{{-- toastr end --}}
+
+
+{{-- dropify --}}
+<script src="{{ asset('assets/custom/js/dropify.min.js') }}"></script>
+<script>
+    $(document).ready(function() {
+        $('.dropify').dropify();
+    });
+</script>
+
+<script>
+    // Set CSRF token for AJAX requests
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+</script>
+
+{{-- sweetalert --}}
+<script src="{{asset('assets/custom/js/sweetalert2@11.js')}}"></script>
+
+@stack('scripts')
--- resources/views/backend/partials/styles.blade.php
+++ resources/views/backend/partials/styles.blade.php
@@ -7,6 +7,48 @@
 <link rel="stylesheet" href="{{ asset('assets/backend/css/theme.min.css') }}">
 
 
+
+{{-- development ................................................. --}}
+{{-- toastr --}}
+<link rel="stylesheet" href="{{ asset('assets/custom/css/toastr.css') }}">
+{{-- push --}}
+</script>
+
+
+{{-- dropify --}}
+<link rel="stylesheet" href="{{ asset('assets/custom/css/dropify.min.css') }}">
+
+
+{{-- sweet alert --}}
+<style>
+    .my-popup-class {
+        background-color: #fffefe;
+        border-radius: 10px;
+        border: 2px solid #f5f7fa;
+    }
+
+    .my-title-class {
+        color: #e5780b;
+        font-size: 24px;
+    }
+
+    .my-content-class {
+        color: #003cc7 !important;
+        font-size: 16px;
+    }
+
+    .my-confirm-button-class {
+        background-color: #25b003;
+        color: black;
+        border-radius: 5px;
+        border: none;
+    }
+</style>
+
+@stack('styles')
+
+
+
 {{-- custom style --}}
 <style>
     :root {
